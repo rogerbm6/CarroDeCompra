24d1fe976f69e89fe83897f7f7565ef2f6a87a91
juanCarlos_acostaPeraba <jcacostaperaba98@gmail.com>
2020-01-20 18:51:00 +0000

    Tarea de teresita | html del index

---
+++ js/master.js
@@ -0,0 +1,11 @@
+class articulo {
+  constructor(nombre = '', precio = 0, canttidad = 0) {
+    this.nombre = nombre;
+    this.precio = precio;
+    this.cantidad = cantidad;
+  }
+
+  
+}
+
+let array
