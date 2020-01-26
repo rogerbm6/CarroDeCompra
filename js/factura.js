68bd3dd65ad1c22e7ba70eeab98ef5c66e4fd34b
juanCarlos_acostaPeraba <jcacostaperaba98@gmail.com>
2020-01-26 19:48:03 +0000

    Falta alert

---
+++ js/factura.js
@@ -0,0 +1,34 @@
+//Array con objetos imágenes
+let arrayObjetosImg = [];
+
+let tuplas = document.getElementById('tuplas');
+
+let precio_total = document.getElementById('precio_total');
+
+let precioT = 0;
+
+for (x = 0; x <= localStorage.length - 1; x++) {
+  let clave = localStorage.key(x);
+  let producto_local = JSON.parse(localStorage.getItem(clave));
+  //document.write("La clave " + clave + "contiene el valor " + localStorage.getItem(clave) + "");
+  let nombre = producto_local.nombre;
+  let categoria = producto_local.categoria;
+  let cantidad = producto_local.cantidad;
+  let precio = producto_local.precio;
+  let ruta = producto_local.ruta;
+  //
+  arrayObjetosImg.push(new Articulo(nombre, categoria, cantidad, precio, ruta));
+}
+
+arrayObjetosImg.forEach((imagen) => {
+  let precioUnidad = imagen.getPrecio();
+  let precioCantidad = imagen.getPrecio() * imagen.getCantidad();
+  tuplas.insertAdjacentHTML("beforeBegin",'<td><img style="width:100px;" class="img-fluid" src=' + imagen.getRuta() + '></td><td>' + imagen.getNombre() + '</td><td>' + imagen.getCategoria() + '</td><td>' + imagen.getCantidad() + '</td><td>' + (precioCantidad) + '</td>');
+  precioT += precioCantidad;
+});
+
+precio_total.innerHTML = precioT;
+
+document.getElementById('volver').addEventListener('click', function() {
+  localStorage.clear();
+})
